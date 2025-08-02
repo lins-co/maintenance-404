76f51668bd61c2c78d5bae31d0114264dcde1009
lins-co <programmerlins@gmail.com>
2025-08-02 16:17:10 +0530

    resume

---
+++ app/resume/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ResumePage = () => (
+  <iframe
+    src="/Lins_Resume (1).pdf"
+    title="Resume"
+    width="100%"
+    height="100%"
+    style={{ border: "none", position: "fixed", top: 0, left: 0, width: "100vw", height: "100vh", margin: 0, padding: 0 }}
+  />
+);
+
+export default ResumePage;
